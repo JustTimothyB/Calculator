5840ced3e1b79431e864bb1379c3c147b0d50747
JustTimothyB <106408706+JustTimothyB@users.noreply.github.com>
2026-07-12 21:09:29 -0400

    Add clear button functionality
    - Added Clear button event listener.
    - Reset calculator display to 0 when clicked.

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+// these select the HTML elements the calculator needs to interact with.
 const numberButtons = document.querySelectorAll('.number');
+const clearButton = document.querySelector('.clear');
 const output = document.querySelector('output');
 
+// handle the number button input.
 numberButtons.forEach((button) => {
     button.addEventListener('click', (event) => {
         const clickedNumber = event.target.textContent;
 
+
+        // replace the initial zero, otherwise append the new digit.
         if (output.textContent === '0') {
             output.textContent = clickedNumber;
         } else {
             output.textContent += clickedNumber;
         }
     });
+});
+
+// reset the calculator display.
+clearButton.addEventListener('click', () => {
+    output.textContent = '0';
 });
