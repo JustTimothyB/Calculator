0ae905aa9b0e1fa96842c9b3a3d3fb23b1bdc7df
JustTimothyB <106408706+JustTimothyB@users.noreply.github.com>
2026-07-13 14:01:15 -0400

    Implemented basic Addition functionality
    - Added operator selection and state tracking.
    - Store first operand and selected operator.
    - Accept second number after operator input.
    - Implemented equals button for addition.

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-// these select the HTML elements the calculator needs to interact with.
+// HTML Element References
 const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
 const clearButton = document.querySelector('.clear');
 const output = document.querySelector('output');
+const equalButton = document.querySelector('.equal');
+
+// Calculator State
+let firstNumber = null;
+let selectedOperator = null;
+let waitingForSecondNumber = false;
 
 // handle the number button input.
 numberButtons.forEach((button) => {
@@ -10,12 +17,33 @@ numberButtons.forEach((button) => {
 
 
         // replace the initial zero, otherwise append the new digit.
-        if (output.textContent === '0') {
+        if (output.textContent === '0' ||
+            waitingForSecondNumber === true
+        ) {
             output.textContent = clickedNumber;
-        } else {
+            waitingForSecondNumber = false;
+        } 
+        else {
             output.textContent += clickedNumber;
         }
     });
+});
+
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        firstNumber = Number(output.textContent);
+        selectedOperator = event.target.textContent;
+        waitingForSecondNumber = true;
+    })
+})
+
+equalButton.addEventListener('click', () => {
+    const secondNumber = Number(output.textContent);
+
+    if (selectedOperator === '+'){
+        const result = firstNumber + secondNumber;
+        output.textContent = result;
+    }
 });
 
 // reset the calculator display.
