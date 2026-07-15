6674aa61f900468dc6c80ffa92bea7f93b0cb125
JustTimothyB <106408706+JustTimothyB@users.noreply.github.com>
2026-07-15 16:51:18 -0400

    Added keyboard navigation and Backspace/ Delete key support
    - Added keyboard support for numbers, operators, decimal, equals, clear and backspace/ delete.
    
    - Added backspace/ delete functionality to remove 1 number at a time.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
 const clearButton = document.querySelector('.clear');
+const backspaceButton = document.querySelector('.backspace');
 const output = document.querySelector('output');
 const equalButton = document.querySelector('.equal');
 const decimalButton = document.querySelector('.decimal');
@@ -73,6 +74,48 @@ decimalButton.addEventListener('click', () => {
     }
 
     output.textContent += '.';
+})
+
+document.addEventListener('keydown', (event) => {
+    const key = event.key;
+
+    if (key >= '0' && key <= '9') {
+        const numberButton = [...numberButtons].find(
+            (button) => button.textContent === key);
+
+            numberButton.click();
+    }
+
+    if (key === '.'){
+        decimalButton.click();
+    }
+
+    if (key === '+' || key === '-' || key === '*' || key === '/') {
+        const operatorButton = [...operatorButtons].find(
+            (button) => button.textContent === key);
+            operatorButton.click();
+    }
+
+    if (key === 'Enter' || key === '=') {
+        equalButton.click();
+    }
+
+    if (key === 'Backspace' || key === 'Delete') {
+        backspaceButton.click();
+    }
+
+    if (key === 'Escape') {
+        clearButton.click();
+    }
+
+});
+
+backspaceButton.addEventListener('click', () => {
+    if (output.textContent.length > 1) {
+        output.textContent = output.textContent.slice(0, -1);
+    } else {
+        output.textContent = '0';
+    }
 })
 
 // reset the calculator display.
